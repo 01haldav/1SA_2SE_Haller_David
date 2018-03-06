a190ae2d119a9096401f29ddfaa07893ba024e80
Schularbeit04 <Schularbeit04@rg-me.schule.suedtirol.it>
2018-03-06 12:50:17 +0100

    Info

--- Auto_David_Haller/Auto_David_Haller/Form1.cs
+++ Auto_David_Haller/Auto_David_Haller/Form1.cs
@@ -46,9 +46,35 @@ namespace Auto_David_Haller
 
         private void button1_Click(object sender, EventArgs e)
         {
-            groupBox1.Text = Marke.Text;
+            groupBox1.Text = "Marke:";
+            groupBox1.Text += Marke.Text;
+            groupBox1.Text += "\nFarbe:";
             groupBox1.Text += comboBox1.SelectedItem;
+            groupBox1.Text += "\nPS:";
+            groupBox1.Text += PS.Text;
+            groupBox1.Text += "\nTankgröße:";
+            groupBox1.Text += Tankgröße.Text;
+            groupBox1.Text += "\nVerbrauch:";
+            groupBox1.Text += Verbrauch.Text;
+            groupBox1.Text += "\nTankinhalt:";
+            groupBox1.Text += tanken;
+            groupBox1.Text += "l;
+             groupBox1.Text += fahren;
+
             }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(Tankgröße.Text);
+            progressBar1.Maximum = a;
+            int b =Convert.ToInt32(tanken.Text);
+            progressBar1.Value += b;
+        }
     
     }
 }
